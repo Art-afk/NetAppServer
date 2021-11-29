f512501db34061db885f5a9aaf99ea2f7b60546c
Art-afk <a976632@gmail.com>
2021-11-29 16:54:33 +0800

    add server

--- buzz/server.py
+++ buzz/server.py
@@ -11,23 +11,34 @@ class MyTCPServer(socketserver.BaseRequestHandler):
         self.__shutdown_request = True
         self.__is_shut_down.wait()
     def handle(self):
-            try:
+           # try:
+            while True:
                 self.data = self.request.recv(1024).strip()
                 if self.data == 'shutdown':
-                    servershutdown()                   
+                    self.servershutdown()
+                #FORMAT = '%(asctime)s %(clientip)-15s %(user)-8s %(message)s'
+                #logging.basicConfig(format=FORMAT)
+                #d = {'clientip': self.client_address[0],'user': 'fbloggs'}
+                FORMAT = '%(asctime)s %(clientip)-8s %(message)s'
+                logging.basicConfig(format=FORMAT)
+                d = {'clientip': self.client_address[0] }
+                
+               
+                logger = logging.getLogger('socketserver')
+                logger.warning('Protocol problem: %s', 'connection resettt', extra=d)
 
-                print("{} wrote:".format(self.client_address[0]))
-                print(self.data)
-
-                self.request.sendall(self.data.upper(), )
-            except socketserver.error as e:
-                logging.error("[ERROR] %s\n" % e[1])
-            #HostIP = socket.gethostbyname(hostname)
-            #        listener.bind(HostIP)
-   
+              #  print("{} wrote:".format(self.client_address[0]))
+                dataS = self.data
+                print(dataS)
+                self.request.sendall(dataS + b'\n')
+            # except socketserver.error as e:
+                #    logging.error("[ERROR] %s\n" % e[1])
+                #HostIP = socket.gethostbyname(hostname)
+                #        listener.bind(HostIP)
+    
 
 if __name__ == "__main__":
+          socketserver.TCPServer.allow_reuse_address = True
           with socketserver.TCPServer((HOST,PORT),MyTCPServer) as server:
-            #socketserver.TCPServer.allow_reuse_address = True
             server.serve_forever()
   
